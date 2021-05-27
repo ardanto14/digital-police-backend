988f2a57cf67e85b98fe27ec68baae9de6734bb3
ardanto.finkan <ardanto.finkan@ui.ac.id>
2021-05-27 18:50:36 +0700

    upload video

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,8 @@
 FROM python:3
 ENV PYTHONUNBUFFERED=1
 WORKDIR /code
+RUN apt update
+RUN apt install ffmpeg libsm6 libxext6  -y
 COPY requirements.txt /code/
 RUN pip install -r requirements.txt
 COPY . /code/
--- main/views.py
+++ main/views.py
@@ -7,6 +7,12 @@ from rest_framework.exceptions import NotFound
 from rest_framework import status
 from fcm_django.models import FCMDevice
 import cv2
+import os
+from .c3d import C3D
+from tensorflow.keras import Model
+from .sports1M_utils import preprocess_input
+import numpy as np
+import tensorflow as tf
 
 class HistorySpecificView(APIView):
 
@@ -48,14 +54,70 @@ class VideoView(APIView):
 
         devices.send_message(title="Title", body="Message")
 
-        return Response()
+        return Response()   
 
 def video_view(request):
     if request.method == 'POST':
-        vid = request.POST['video']
-        cap = cv2.VideoCapture(vid)
+        vid = request.FILES.get('video', None).read()
+        # print(vid)
+        f = open('output.mp4', 'wb')
+        f.write(vid)
+        f.close()
+        cap = cv2.VideoCapture('output.mp4')
 
-        print(type(vid))
+        all_frame = []
+        while True:
+            ret, frame = cap.read()
+            if not ret:
+                break
+            all_frame.append(frame)
+        cap.release()
 
-        return redirect('main')
+        ln = len(all_frame)
+        
+        base_model = C3D(weights='sports1M')
+        feature_extractor = Model(inputs=base_model.input, outputs=base_model.get_layer('fc6').output)
+
+
+        prediction_model = tf.keras.Sequential([
+            tf.keras.layers.Input(shape=(4096,)),
+            tf.keras.layers.Dropout(0.6),
+            tf.keras.layers.Dense(512, kernel_initializer='glorot_normal', kernel_regularizer=tf.keras.regularizers.L2(0.001), activation='relu'),
+            tf.keras.layers.Dropout(0.6),
+            tf.keras.layers.Dense(32, kernel_initializer='glorot_normal', kernel_regularizer=tf.keras.regularizers.L2(0.001)),
+            tf.keras.layers.Dropout(0.6),
+            tf.keras.layers.Dense(1, kernel_initializer='glorot_normal', kernel_regularizer=tf.keras.regularizers.L2(0.001), activation='sigmoid'),
+        ])
+
+        prediction_model.load_weights('test_model.h5')
+
+        model = tf.keras.Sequential([
+            feature_extractor,
+            tf.keras.layers.Flatten(),
+            prediction_model
+        ])
+
+        # Select 16 frames from video
+        start = 0
+        is_anomaly = False
+        while True:
+            if start + 16 > ln:
+                break
+
+            vid = np.array(all_frame[start:start+16])
+            start += 16
+
+            x = preprocess_input(vid)
+
+            features = model.predict(x)
+
+            if features[0][0] > 0.5:
+                is_anomaly = True
+                break
+            print(features)
+
+
+        os.remove('output.mp4')
+
+        return render(request, 'main/main.html', {'is_anomaly': is_anomaly})
     return render(request, 'main/main.html')
